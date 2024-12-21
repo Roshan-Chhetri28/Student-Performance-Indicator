e09a66bd6bb134f002912b0506498dd2418227fa
roshan chhetri <roshanchhetri931@gmail.com>
2024-12-22 01:46:09 +0530

    initial commit

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import numpy as np
 from src.logger import logging
 from src.exception import CustomException
 import dill
-
+from sklearn.metrics import r2_score
 def save_obj(file_path, obj):
         try:
             dir_path = os.path.dirname(file_path)
@@ -19,3 +19,23 @@ def save_obj(file_path, obj):
 
         except:
             pass
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+            report = {}
+            for i in range(len(models)):
+                  model = list(models.values())[i]
+
+                  model.fit(X_train, y_train)
+                  y_train_pred = model.predict(X_train)
+                  y_test_pred = model.predict(X_test)
+                  
+
+                  train_model_score = r2_score(y_train, y_train_pred)
+
+                  test_model_score = r2_score(y_test, y_train_pred)
+
+                  report[list(models.keys())[i]] = test_model_score
+            return report
+    except:
+          pass              
+    
