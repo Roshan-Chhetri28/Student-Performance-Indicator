22616a2fc25145fc5cac4eb79906130d42101cd9
roshan chhetri <roshanchhetri931@gmail.com>
2024-12-23 15:52:15 +0530

    Hyper parameter tuning

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ from src.logger import logging
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 def save_obj(file_path, obj):
         try:
             dir_path = os.path.dirname(file_path)
@@ -19,23 +20,31 @@ def save_obj(file_path, obj):
 
         except Exception as e:
             raise CustomException(e, sys)
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train,X_test,y_test,models,params):
     try:
-            report = {}
-            for i in range(len(models)):
-                  model = list(models.values())[i]
+        report = {}
 
-                  model.fit(X_train, y_train)
-                  y_train_pred = model.predict(X_train)
-                  y_test_pred = model.predict(X_test)
-                  
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param=params[list(models.keys())[i]]
 
-                  train_model_score = r2_score(y_train, y_train_pred)
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
 
-                  test_model_score = r2_score(y_test, y_train_pred)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-                  report[list(models.keys())[i]] = test_model_score
-            return report
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
     except Exception as e:
           raise CustomException(e, sys)   
     
